ded714c7a18c97b5ff4ab03e8f50dd66fc980d5d
Rafael Giaqueto <81272037+Rafaelgiaqueto@users.noreply.github.com>
2023-06-21 20:00:40 -0300

    Feature/complete-project-frontend

--- backend/src/services/breakdownsService.ts
+++ backend/src/services/breakdownsService.ts
@@ -1,5 +1,7 @@
 import { BadRequestError, NotFoundError } from "../helpers/api-erros";
 import { breakdownsRepository } from "../repositories/breakdownsRepository";
+import { categoryRepository } from "../repositories/categoryRepository";
+import { localPatrimonyRepository } from "../repositories/localPatrimonyRepository";
 import { MaintenanceRepository } from "../repositories/maintenanceRepository";
 import { patrimonyRepository } from "../repositories/patrimonyRepository";
 
@@ -63,34 +65,27 @@ export class BreakdownsService {
   
   
 
-  async updateBreakdown(id_avaria: string, breakdownData: any) {
-    const breakdown = await breakdownsRepository.findOne({where: {id_avaria}});
-    if (!breakdown) {
-      throw new NotFoundError("Avaria não encontrada");
+  async updatePatrimony(id_patrimonio: string, patrimonyData: any) {
+    const category = await categoryRepository.findOne({ where: { id_categoria: patrimonyData.id_categoria } });
+    if (!category) {
+      throw new BadRequestError("Categoria não encontrada");
     }
-
-    const patrimony = await patrimonyRepository.findOne({
-        where: {id_patrimonio: breakdownData.id_patrimonio}
-    })
-    if(!patrimony){
-        throw new BadRequestError('Patrimônio não encontrado')
+  
+    const local = await localPatrimonyRepository.findOne({ where: { id_local_patrimonio: patrimonyData.id_local_patrimonio } });
+    if (!local) {
+      throw new BadRequestError("Local de patrimônio não encontrado");
     }
-
-    const maintenance = await MaintenanceRepository.findOne({
-        where: {id_manutencao: breakdownData.id_manutencao}
-    })
-    if(!maintenance) {
-        throw new BadRequestError('Manutenção não encontrada')
-    }
-
-    Object.assign(breakdown, {
-        ...breakdownData,
-        patrimony,
-        maintenance
-    })
-
-    await breakdownsRepository.save(breakdown)
+  
+    const updatedPatrimony = {
+      ...patrimonyData,
+      category,
+      localPatrimony: local,
+    };
+  
+    await patrimonyRepository.update(id_patrimonio, updatedPatrimony);
   }
+  
+  
 
   async deleteBreakdown(id_avaria: string) {
     const breakdown = await breakdownsRepository.findOne({where: {id_avaria}});
